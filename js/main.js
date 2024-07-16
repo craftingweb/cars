efb01a699adddee43227bfcca76bc2c1bf7802a7
VitaliPri <vitaliy.prymak@cix.csi.cuny.edu>
2024-07-16 12:19:34 -0400

    slider mobile friendly

--- js/main.js
+++ js/main.js
@@ -3,5 +3,25 @@ $(function () {
     arrows: false,
     dots: true,
     slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 550,
+        settings: {
+          slidesToShow: 2,
+          slideToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 450,
+        settings: {
+          slidesToShow: 1,
+          slideToScroll: 2,
+          infinite: true,
+          dots: true,
+        },
+      },
+    ],
   });
 });
